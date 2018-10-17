cf6079dc53e229957c8f997c461aad7603975dbf
omnitrogen <felixgif@gmail.com>
2018-10-17 02:10:06 +0200

    new version of the website

---
+++ js/index.js
@@ -0,0 +1,22 @@
+let menuToggle = document.querySelector('.toggle-menu');
+let firstLine = document.querySelector('.first');
+let secondLine = document.querySelector('.second');
+let thirdLine = document.querySelector('.third');
+let menu = document.querySelector('#main-nav');
+var x = 0;
+document.getElementById('main-nav').style.display = "none";
+
+
+menuToggle.addEventListener('click', (e) => {
+    e.preventDefault();
+    firstLine.classList.toggle('rotate-first');
+    secondLine.classList.toggle('hide-second');
+    thirdLine.classList.toggle('rotate-third');
+    menu.classList.toggle('display-menu');
+    if (window.x % 2 == 0) {
+        document.getElementById('main-nav').style.display = "flex";
+    } else {
+        document.getElementById('main-nav').style.display = "none";
+    }
+    x++;
+});
